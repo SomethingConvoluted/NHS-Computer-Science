9ca36890fd239ef347e271b960b480e05d1397db
SomethingConvoluted <47113144+SomethingConvoluted@users.noreply.github.com>
2019-02-22 12:15:27 -0500

    Create lightsout.js

---
+++ lightouts/lightsout.js
@@ -0,0 +1,33 @@
+html>
+<body>
+<table>
+
+
+
+</table>
+<script>
+(style
+</script>
+<h2>The Button Element</h2>
+<button type="button" onclick="changecolortothis(#FF0000)">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<br>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<br>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<br>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+
+</body>
+</html>
