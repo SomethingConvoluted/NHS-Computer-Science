584eff38440b656679b6bbb0275016695f503434
SomethingConvoluted <47113144+SomethingConvoluted@users.noreply.github.com>
2019-04-08 00:17:38 -0400

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>The conncect 4 element</h2>
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.dot {
-  height: 45px;
-  width: 45px;
-  background-color: #bbb;
-  border-radius: 70%;
-  display: inline-block;
+ display: inline-block;
 }
 </style>
 
@@ -26,70 +13,72 @@ function toggle(b) {
   	document.getElementById(b).style.backgroundColor = "blue";
   }
 }
+//
+
 </script>
 
 <div style="text-align:center">
 
   <span class="dot" id="b1" onclick="toggle('b1')" style='background-color:lightgray;'></span>
-  <span class="dot" id="b2"></span>
-  <span class="dot" id="b3"></span>
-  <span class="dot" id="b4"></span>
-  <span class="dot" id="b5"></span>
-  <span class="dot" id="b6"></span>
-  <span class="dot" id="b7"></span>
+  <span class="dot" id="b2" onclick="toggle('b2')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b3" onclick="toggle('b3')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b4" onclick="toggle('b4')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b5" onclick="toggle('b5')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b6" onclick="toggle('b6')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b7" onclick="toggle('b7')" style='background-color:lightgray;'></span>
   <div>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
+  <span class="dot "id="b8" onclick="toggle('b8')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b9" onclick="toggle('b9')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b10" onclick="toggle('b10')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b11" onclick="toggle('b11')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b12" onclick="toggle('b12')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b13" onclick="toggle('b13')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b14" onclick="toggle('b14')" style='background-color:lightgray;'></span>
   </div>
   <div>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
+  <span class="dot" id="b15" onclick="toggle('b15')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b16" onclick="toggle('b16')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b17" onclick="toggle('b17')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b18" onclick="toggle('b18')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b19" onclick="toggle('b19')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b20" onclick="toggle('b20')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b21" onclick="toggle('b21')" style='background-color:lightgray;'></span>
   </div>
   <div>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
+  <span class="dot" id="b22" onclick="toggle('b22')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b23" onclick="toggle('b23')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b24" onclick="toggle('b24')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b25" onclick="toggle('b25')" style='background-color:lightgray;'></span>
+  <span class="dot" id="26" onclick="toggle('b26')" style='background-color:lightgray;'></span>
+  <span class="dot" id="27" onclick="toggle('b27')" style='background-color:lightgray;'></span>
+  <span class="dot" id="28" onclick="toggle('b28')" style='background-color:lightgray;'></span>
   </div>
   <div>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
+  <span class="dot" id="b29" onclick="toggle('b29')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b30" onclick="toggle('b30')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b31" onclick="toggle('b31')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b32" onclick="toggle('b32')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b33" onclick="toggle('b33')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b34" onclick="toggle('b34')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b35" onclick="toggle('b35')" style='background-color:lightgray;'></span>
   </div>
   <div>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
+  <span class="dot" id="b36" onclick="toggle('b36')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b37" onclick="toggle('b37')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b38" onclick="toggle('b38')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b39" onclick="toggle('b39')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b40" onclick="toggle('b40')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b41" onclick="toggle('b41')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b42" onclick="toggle('b42')" style='background-color:lightgray;'></span>
  </div>
  <div>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
+  <span class="dot" id="b43" onclick="toggle('b43')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b44" onclick="toggle('b44')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b45" onclick="toggle('b45')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b46" onclick="toggle('b46')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b47" onclick="toggle('b47')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b48" onclick="toggle('b48')" style='background-color:lightgray;'></span>
+  <span class="dot" id="b49" onclick="toggle('b49')" style='background-color:lightgray;'></span>
   </div>
   
 </body>
